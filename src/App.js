f35bfa1a77a5a2322f37769ecd402dfedcd4ae03
RBhownath <rbhownath@csir.co.za>
2022-07-02 22:48:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect,useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'; //run-> npm install --save bootstrap@^4.0.0-alpha.6  react-bootstrap@^0.32.1
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+ 
+
+ 
 function App() {
+
+  const [width, setWindowWidth] = useState(0)
+   useEffect(() => { 
+
+     updateDimensions();
+
+     window.addEventListener('resize', updateDimensions);
+     return () => 
+       window.removeEventListener('resize',updateDimensions);
+    }, [])
+
+    const updateDimensions = () => {
+      const width = window.innerWidth
+      setWindowWidth(width)
+      console.log("size = " + width)
+    }
+
+    const showNav= {
+      display: width>200 ? 'flex' : 'none'
+    }
+    const showMenuIcon = {
+      display: width>200 ? 'none' : 'flex',
+    }
+
+  if (width>400){  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div><h1>Rinel big</h1></div>
   );
+  }else{
+
+  return (
+    <div><h1>Rinel samll</h1></div>
+   );
+  }
+
 }
 
 export default App;
