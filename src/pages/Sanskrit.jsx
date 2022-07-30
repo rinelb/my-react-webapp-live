7cadf6dc428ead429ac56ecd83e76c841a460a27
RBhownath <rbhownath@csir.co.za>
2022-07-30 12:13:06 +0200

    made sanskrit page look better by adding sections

--- src/pages/Sanskrit.jsx
+++ src/pages/Sanskrit.jsx
@@ -3,7 +3,12 @@ import {Button, Col,Row, Form,FormControl, Container} from 'react-bootstrap'
 import Table from 'react-bootstrap/Table';
 import Sanscript from '@indic-transliteration/sanscript';
 import SanskritLetter from  '../data/Sanskrit.json'
- 
+import SanskritCons from  '../data/SankritConst.json'
+import SankritVol from  '../data/SankritVowles.json'
+import Sankrit3 from  '../data/Snakrit3.json'
+import SanskritNum from  '../data/SankritNum.json'
+import SanskritSplV from  '../data/ShankritSplVowle.json'
+import SanskritRest from  '../data/SankritRest.json'
 const bookNumber = 6
 // const maxPageNumber = Math.floor(bookItems.length /6)
 // const maxBooks = bookItems.length
@@ -77,34 +82,111 @@ const Sanskrit = ()=>{
          
             <>
             <center>
-                <br></br>
+                <br/>
              <Form className="d-flex " style={{width:"15em",alignItems:"center"}}  >
                      <FormControl className="text-center" type="text" style={{alignContent:"middle"}} placeholder="Enter Word or Sentance" onChange={updateSankrit}/>
                 </Form> <br></br>
-                <h3>{outputWord}</h3>
+                <h5>{outputWord}</h5>
 
                 <hr></hr>
             </center>
              { (mobile==1) ?
              <>
              <center><h1>Sanskrit Mapping Code</h1><br/><br/></center>
-            <Container>     
-                    <Row md={3} xs={3} lg={3} className="g-2  align-items-center"  >
-                        {SanskritLetter.map(letter => (
-                                <Col className="text-center text-md-right bg-light" key={letter.id}><h1> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter} </h1></Col> 
-                         ))} 
-                </Row>
-           </Container>
+                    <Container>  
+                        <Row md={4} xs={4} lg={4} className="g-3 align-items-center "  >
+                            {SankritVol.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+                                <br></br>
+                                <Row md={3} xs={3} lg={3} className="g-3 align-items-center "  >
+                            {Sankrit3.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritCons.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br><br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritSplV.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br><br></br>
+                                <Row md={3} xs={3} lg={3} className="g-3 align-items-center "  >
+                             
+                              {SanskritRest.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+
+                                <br></br><br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritNum.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+                     </Container>
             </> 
             : // PC version
             <>
             <center><h1>Sanskrit Mapping Code</h1><br/><br/></center>
                     <Container>  
-                        <Row md={6} xs={6} lg={6} className="g-3 align-items-center "  >
-                            {SanskritLetter.map(letter => (
-                                
-                                <Col className="text-center text-md-right bg-light" key={letter.id}><h1> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h1></Col>
-                            ))} 
+                        <Row md={4} xs={4} lg={4} className="g-3 align-items-center "  >
+                            {SankritVol.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+                                <br></br>
+                                <Row md={3} xs={3} lg={3} className="g-3 align-items-center "  >
+                            {Sankrit3.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritCons.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br><br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritSplV.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+                                <br></br><br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritRest.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
+                                </Row>
+
+
+                                <br></br><br></br>
+                                <Row md={5} xs={5} lg={5} className="g-3 align-items-center "  >
+                             
+                              {SanskritNum.map(letter => ( 
+                                <Col className="text-center text-md-right bg-light" key={letter.id}><h5> {Sanscript.t(letter.letter, 'hk', 'devanagari')} : {letter.letter}</h5></Col>
+                             ))} 
                                 </Row>
                      </Container>
             </>
